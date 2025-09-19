00ecb9621b67922b3507bc73cf86974aff255d99
Tyler-Barsby <tyler@flowmondo.com>
2025-09-19 17:02:10 +0100

    Add files via upload

---
+++ Apprenticeship-Stuff/hello_world.py
@@ -0,0 +1,32 @@
+
+#====================================
+
+data = "cloudacademy.python.2019"
+letter1 = 'a'
+word1 = 'cloud'
+num1 = '2019'
+
+#CODE1: Count occurrences of letters and words
+print("CODE1:")
+print(f"count '{letter1}' = {data.count(letter1)}")
+print(f"count '{word1}' = {data.count(word1)}")
+print(f"count '{num1}' = {data.count(num1)}")
+print()
+
+#CODE2: Get length of string
+print("CODE2:")
+length = len(data)
+print(f"len '{data}' = {length}")
+print()
+
+#CODE3: Get max of string - the highest char in the string
+print("CODE3:")
+maximum = max(data)
+print(f"max '{data}' = {maximum}")
+print()
+
+#CODE4: Get min of string - the lowest char in the string
+print("CODE4:")
+minimum = min(data)
+print(f"min '{data}' = {minimum}")
+print()
